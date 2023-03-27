1f7d9a3a1ae9f1d314fec14ebc3d5d0c4e0b6861
Ajay Sharma <ajshrmaofficial@gmail.com>
2023-03-27 18:16:00 +0530

    project structure initialised

---
+++ src/components/App.jsx
@@ -0,0 +1,18 @@
+import './css/App.css'
+
+const App = () => {
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+  }
+
+  return (
+    <div>
+      <h1>MessageMe</h1>
+      <form onSubmit={sendMessage}>
+        <input type="text" placeholder='Type Message' />
+        <button type='submit'>Send</button>
+      </form>
+    </div>
+  )
+}
