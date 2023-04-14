235aeb314d0d4936bcd79e68ed9aa5cdd23952f5
Ajay Sharma <ajshrmaofficial@gmail.com>
2023-04-14 10:41:07 +0530

    username added

---
+++ src/components/Broadcaster.jsx
@@ -0,0 +1,48 @@
+import { useContext, useEffect, useState } from "react";
+import { UserData } from "./Context";
+import chatSocket from "../socket";
+import "../css/App.css";
+
+const Broadcaster = () => {
+  const user = useContext(UserData)
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    const onChat = ({message, fromID, fromUsername}) => {
+      const messageList = document.querySelector("#messageList");
+      const li = document.createElement("li");
+      li.textContent = `#${fromUsername}: ` + message;
+      messageList.appendChild(li);
+    };
+
+    chatSocket.on("chat message", onChat);
+
+    return () => {
+      chatSocket.off('chat message', onChat)
+    }
+  }, []);
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+    chatSocket.emit("chat message", {message, fromID:chatSocket.id, fromUsername:chatSocket.auth.username});
+    setMessage("")
+  };
+
+  return (
+    <div>
+      <h1>Guftgu</h1>
+      <form onSubmit={sendMessage}>
+        <input
+          type="text"
+          placeholder="Type Message"
+          onChange={(e) => setMessage(e.target.value)}
+          value={message}
+        />
+        <button type="submit">Send</button>
+      </form>
+      <ul id="messageList"></ul>
+    </div>
+  );
+};
+
+export default Broadcaster;
