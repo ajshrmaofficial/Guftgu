893dc48b883ecd096f9e08a989986e13b4999cc2
Ajay Sharma <ajshrmaofficial@gmail.com>
2023-04-14 10:43:00 +0530

    username implementation on broadcasting done

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
-import { Server } from "socket.io";
-const  app = require('express')();
+// const  server = require('express')();
+const io = require('socket.io')({
+    cors:{
+        origin: ["http://localhost:5173", "http://192.168.2.11:5173"]
+    }})
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// const cors = require('cors');
+// server.use(express.json());
+// server.use(express.urlencoded({ extended: true }));
+// server.use(cors({
+//     origin: "http://localhost:5173"
+// }));
 
-const io = Server({
-    cors
+
+chatNamespace = io.of('/chat');
+
+chatNamespace.on('connection', (socket)=>{
+    console.log(`A user connected to chat namespace: ${socket.id} ${socket.username}`)
+    socket.on('chat message', ({message, fromID, fromUsername})=>{
+        console.log(`Recieved message from ${socket.username}: `, message)
+        chatNamespace.emit('chat message', {message, fromID, fromUsername})
+    })
 })
 
-app.listen(3000, ()=>{
-    console.log("Server is running on port 3000")
+chatNamespace.use((socket, next)=>{
+    const username = socket.handshake.auth.username
+    if(!username){
+        return next(new Error("invalid username"))
+    }
+    socket.username = username
+    next()
 })
+
+io.listen(3000)
+
+// server.listen(3000, ()=>{
+//     console.log("Server is running on port 3000")
+// })
