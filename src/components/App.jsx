76ff2d0ca791ea7171921c4a9f0ce622f7889185
Ajay Sharma <ajshrmaofficial@gmail.com>
2023-04-14 10:39:06 +0530

    broadcaster logic implemented

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,75 @@
-import './css/App.css'
+import { useContext, useEffect, useState } from "react";
+import { UserData } from "./Context";
+import chatSocket from "../socket";
+import Broadcaster from "./Broadcaster";
 
 const App = () => {
+  const [username, setUsername] = useState("");
+  const [usernameSelected, setUsernameSelected] = useState(false);
+  const [isConnected, setIsConnected] = useState(chatSocket.connected);
 
-  const sendMessage = (e) => {
-    e.preventDefault();
-  }
+  useEffect(() => {
+    function onConnect() {
+      setIsConnected(true);
+			setUsernameSelected(true);
+			console.log("Connected to socket server")
+    }
+
+    function onDisconnect() {
+      setIsConnected(false);
+			setUsernameSelected(false);
+			console.log("Disconnected to socket server")
+    }
+
+    function onUsernameError(err) {
+      setUsernameSelected(false);
+      setIsConnected(false);
+      console.log(`Username error Log: ${err.message}`);
+    }
+
+    chatSocket.on("connect", onConnect);
+    chatSocket.on("disconnect", onDisconnect);
+    chatSocket.on("connect_error", (err) => {
+      onUsernameError(err);
+    });
+
+    return () => {
+      chatSocket.off("connect", onConnect);
+      chatSocket.off("disconnect", onDisconnect);
+      chatSocket.off("connect_error", onUsernameError);
+    };
+  }, []);
 
   return (
-    <div>
-      <h1>MessageMe</h1>
-      <form onSubmit={sendMessage}>
-        <input type="text" placeholder='Type Message' />
-        <button type='submit'>Send</button>
-      </form>
-    </div>
-  )
-}
+    <UserData.Provider value={{ username, setUsername }}>
+      {(isConnected && usernameSelected) ? <Broadcaster /> : <Login />}
+    </UserData.Provider>
+  );
+};
 
-export default App
+const Login = () => {
+  const user = useContext(UserData);
+
+  const submit = (e) => {
+    e.preventDefault();
+		const username = user.username
+    chatSocket.auth = { username };
+    chatSocket.connect();
+  };
+
+  return (
+    <>
+       <h3>Login</h3>
+      <input
+        type="text"
+        placeholder="Enter username"
+        onChange={(e) => {
+          user.setUsername(e.target.value);
+        }}
+      />
+      <button onClick={submit}>Enter</button>
+    </>
+  );
+};
+
+export default App;
