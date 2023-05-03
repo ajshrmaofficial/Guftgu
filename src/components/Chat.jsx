59b8341c6da8a5f983a8462bc4d79b7a01036ded
Ajay Sharma <ajshrmaofficial@gmail.com>
2023-05-03 22:14:02 +0530

    starting from scratch

--- src/components/Chat.jsx
+++
@@ -1,9 +0,0 @@
-import Login from "./Login"
-
-const Chat = () => {
-    return(
-        <div>chat interface</div>
-    )
-}
-
-export default Chat
--- src/index.jsx
+++ src/index.jsx
@@ -1,38 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 import App from './components/App'
 import './css/index.css'
-import Chat from './components/Chat'
-import ErrorPage from './components/ErrorPage'
-import Login from './components/Login'
-import Guftgu from './components/Guftgu'
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App/>,
-    errorElement: <ErrorPage/>,
-    children: [
-      {
-        path: '/',
-        element: <Chat/>,
-        errorElement: <ErrorPage/>
-      },
-      {
-        path: '/guftgu',
-        element: <Guftgu/>,
-        errorElement: <ErrorPage/>
-      },
-      {
-        path: '/login',
-        element: <Login/>,
-        errorElement: <ErrorPage/>
-      }
-    ]
-  }
-])
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <RouterProvider router={router}/>
+ReactDOM.createRoot(document.getElementById('root')).render(    
+      <App/>
 )
