793e3a0ca9925157c7d2a87bbc9631032a02560d
Ajay Sharma <ajshrmaofficial@gmail.com>
2023-05-07 08:54:29 +0530

    login state check added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,26 @@
 import { Outlet } from "react-router-dom"
 import authContext from "../utility/authContext"
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import server from "../utility/serverConfig"
+import {setSocketUsername, connectChatSocket} from "../utility/socket"
 
 function App() {
     const [isAuthenticated, setIsAuthenticated] = useState(false)
 
+    
+    useEffect(()=>{
+        const isLoggedIn = async() => {
+            const response = await server.get('/auth/isLoggedIn', {withCredentials: true})
+            if(response.data?.isLoggedIn){
+                const userData = JSON.parse(localStorage.getItem('userData'))
+                setSocketUsername(userData.username)
+                connectChatSocket()
+                setIsAuthenticated(true)
+            }
+        }
+        if(!isAuthenticated) isLoggedIn()
+    },[])
+    
     return(
         <authContext.Provider value={{isAuthenticated, setIsAuthenticated}}>
         <h1>App file</h1>
