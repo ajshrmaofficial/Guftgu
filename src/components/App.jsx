9032023987b7fda8b9f5b56acf10a334fe46040e
Ajay Sharma <ajshrmaofficial@gmail.com>
2023-05-10 11:40:19 +0530

    added try catch in async function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,32 +1,37 @@
-import { Outlet } from "react-router-dom"
-import authContext from "../utility/authContext"
-import { useEffect, useState } from "react"
-import server from "../utility/serverConfig"
-import {setSocketUsername, connectChatSocket} from "../utility/socket"
+import { Outlet } from "react-router-dom";
+import authContext from "../utility/authContext";
+import { useEffect, useState } from "react";
+import server from "../utility/serverConfig";
+import { setSocketUsername, connectChatSocket } from "../utility/socket";
 
 function App() {
-    const [isAuthenticated, setIsAuthenticated] = useState(false)
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-    
-    useEffect(()=>{
-        const isLoggedIn = async() => {
-            const response = await server.get('/auth/isLoggedIn', {withCredentials: true})
-            if(response.data?.isLoggedIn){
-                const userData = JSON.parse(localStorage.getItem('userData'))
-                setSocketUsername(userData.username)
-                connectChatSocket()
-                setIsAuthenticated(true)
-            }
+  useEffect(() => {
+    const isLoggedIn = async () => {
+      try {
+        const response = await server.get("/auth/isLoggedIn", {
+          withCredentials: true,
+        });
+        if (response.data?.isLoggedIn) {
+          const userData = JSON.parse(localStorage.getItem("userData"));
+          setSocketUsername(userData.username);
+          connectChatSocket();
+          setIsAuthenticated(true);
         }
-        if(!isAuthenticated) isLoggedIn()
-    },[])
-    
-    return(
-        <authContext.Provider value={{isAuthenticated, setIsAuthenticated}}>
-        <h1>App file</h1>
-            <Outlet/>
-        </authContext.Provider>
-    )
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    if (!isAuthenticated) isLoggedIn();
+  }, []);
+
+  return (
+    <authContext.Provider value={{ isAuthenticated, setIsAuthenticated }}>
+      <h1>App file</h1>
+      <Outlet />
+    </authContext.Provider>
+  );
 }
 
-export default App
+export default App;
