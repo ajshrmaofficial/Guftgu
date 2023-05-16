61952c008d5b8f322879b40087fce86be4c767db
Ajay Sharma <ajshrmaofficial@gmail.com>
2023-05-16 12:10:17 +0530

    moved mongodb uri to env

--- auth/authRouter.js
+++ auth/authRouter.js
@@ -9,7 +9,7 @@ const redisClient = sessionManager.redisClient;
 
 async function connectMongoose() {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/userDB");
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("connected to mongoose database");
   } catch (err) {
     console.log(err);
