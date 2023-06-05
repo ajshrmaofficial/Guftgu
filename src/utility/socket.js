4b195e1548a86aa976fdbbef08fd655593b61318
Ajay Sharma <ajshrmaofficial@gmail.com>
2023-06-05 16:58:31 +0530

    adding secure option to socket for production env

--- src/utility/socket.js
+++ src/utility/socket.js
@@ -1,9 +1,11 @@
 import { io } from "socket.io-client";
 
 const URL = import.meta.env.VITE_SERVER_IP
+const env = import.meta.env.VITE_ENV
 const chatSocket = io(URL + '/chat', {
     autoConnect: false,
     withCredentials: true,
+    secure: env === 'production' ? true : false,
 })
 
 function setSocketUsername(username){
