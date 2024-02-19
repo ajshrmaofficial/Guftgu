59a2f0a5d2311f40ed6d47fb4aa64749ec086b72
Ajay Sharma <ajshrmaofficial@gmail.com>
2024-02-20 01:44:16 +0530

    INIT: initialized mehfil screen

---
+++ packages/app/src/screens/Mehfil.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+function Mehfil(): React.JSX.Element {
+    return (
+        <View>
+            <Text>
+                Mehfil
+            </Text>
+        </View>
+    )
+}
+
+export default Mehfil;
