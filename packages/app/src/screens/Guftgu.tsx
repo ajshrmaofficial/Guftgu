77b30bb5293cb651c7751c5c898c985db64c08c3
Ajay Sharma <ajshrmaofficial@gmail.com>
2024-02-20 01:43:51 +0530

    INIT: initialized guftgu screen

---
+++ packages/app/src/screens/Guftgu.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+function Guftgu(): React.JSX.Element {
+    return (
+        <View>
+            <Text>
+                Guftgu
+            </Text>
+        </View>
+    )
+}
+
+export default Guftgu;
