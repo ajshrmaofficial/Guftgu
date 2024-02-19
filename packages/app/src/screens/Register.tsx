d742dc627d10daddf178ac11876ed58c1ee065a6
Ajay Sharma <ajshrmaofficial@gmail.com>
2024-02-20 01:44:29 +0530

    INIT: initialized register screen

---
+++ packages/app/src/screens/Register.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+function Register(): React.JSX.Element {
+    return (
+        <View>
+            <Text>
+                Register
+            </Text>
+        </View>
+    )
+}
+
+export default Register;
