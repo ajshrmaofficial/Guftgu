34439d3a92666e35c0a5751f969d5b3774723e8d
Ajay Sharma <ajshrmaofficial@gmail.com>
2024-03-11 11:11:31 +0530

    Fix: reverted nativewind installation to stable v2 from v4

--- packages/app/babel.config.js
+++ packages/app/babel.config.js
@@ -1,6 +1,4 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset', 'nativewind/babel'],
-  plugins: [
-    ['module:react-native-dotenv']
-  ]
+  presets: ['module:@react-native/babel-preset'],
+  plugins: ['module:react-native-dotenv', 'nativewind/babel'],
 };
