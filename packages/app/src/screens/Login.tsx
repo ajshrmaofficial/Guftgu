16ab65ab90e87c86b24addae22bd944a50fa8f9a
Ajay Sharma <ajshrmaofficial@gmail.com>
2024-03-11 11:21:17 +0530

    Feat: added UI and logic for login page

--- packages/app/src/screens/Login.tsx
+++ packages/app/src/screens/Login.tsx
@@ -1,39 +1,61 @@
-import React, { useEffect, useState } from "react";
-import { Text, TextInput, TouchableOpacity, View } from "react-native";
-import "../../global.css"
-import { useAuth } from "../utility/context/AuthContext";
-import { NavStackProps } from "../utility/navigation/NavigationStackTypes";
+import React, {useState} from 'react';
+import {Text, TextInput, TouchableOpacity, View} from 'react-native';
+import {useAuth} from '../utility/context/AuthContext';
+import {NavStackProps} from '../utility/navigation/NavigationStackTypes';
 
-function Login({navigation}: NavStackProps<"Login">): React.JSX.Element {
-    const [username, setUsername] = useState<string>('')
-    const [password, setPassword] = useState<string>('')
+function Login({navigation}: NavStackProps<'Login'>): React.JSX.Element {
+  const [username, setUsername] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
 
-    const {login, loading} = useAuth();
+  const {login, loading, err} = useAuth();
 
-    const onPress = () => {
-       login(username, password);
-    }
+  const submitCredentials = () => {
+    login(username, password);
+  };
 
-    return (
-        <>
-            {loading && <View className="justify-center items-center">
-                            <Text className="text-4xl">Loading...</Text>
-                        </View>
-            }
-            {!loading && <View className="items-center">
-                <Text className="text-4xl absolute m-3 self-start">Login</Text>
-                <View className="w-2/3 h-full justify-center items-center">
-                    <Text className="self-start">Username</Text>
-                    <TextInput className="border border-white rounded-md w-full mb-4" placeholder="Username" value={username} onChangeText={(text) => setUsername(text)} />
-                    <Text className="self-start">Password</Text>
-                    <TextInput className="border border-white rounded-md w-full mb-5" placeholder="Password" value={password} onChangeText={(text) => setPassword(text)} />
-                    <TouchableOpacity className="border border-white rounded-md w-2/3 p-2 items-center" onPress={!loading && onPress}> 
-                        <Text>Submit</Text>
-                    </TouchableOpacity>
-                </View>
-            </View>}
-        </>
-    )
+  const navigateSignup = () => {
+    navigation.navigate('Register');
+  };
+
+  return (
+    <>
+      {loading && (
+        <View>
+          <Text className="text-4xl">Loading...</Text>
+        </View>
+      )}
+      {!loading && (
+        <View className="items-center">
+          <Text className="text-4xl absolute m-3 self-start">Login</Text>
+          <View className="w-2/3 h-full justify-center items-center">
+            <TextInput
+              className="border border-white rounded-md w-full mb-4"
+              placeholder="Username"
+              value={username}
+              onChangeText={text => setUsername(text)}
+            />
+            <TextInput
+              className="border border-white rounded-md w-full mb-5"
+              placeholder="Password"
+              value={password}
+              onChangeText={text => setPassword(text)}
+            />
+            <TouchableOpacity
+              className="border border-white rounded-md w-2/3 p-2 items-center"
+              onPress={!loading && submitCredentials}>
+              <Text>Submit</Text>
+            </TouchableOpacity>
+            <Text>
+              Doesn't have an account?{' '}
+              <TouchableOpacity onPress={navigateSignup}>
+                <Text>SignUp</Text>
+              </TouchableOpacity>
+            </Text>
+          </View>
+        </View>
+      )}
+    </>
+  );
 }
 
 export default Login;
