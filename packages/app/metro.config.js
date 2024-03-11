5d15942a34094daf146b8fd1d14e6ae9bb78812b
Ajay Sharma <ajshrmaofficial@gmail.com>
2024-03-11 11:13:28 +0530

    Fix: reverted nativewind v4 matro config to default metro config

--- packages/app/metro.config.js
+++ packages/app/metro.config.js
@@ -1,5 +1,4 @@
 const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
-const { withNativeWind } = require('nativewind/metro')
 
 /**
  * Metro configuration
@@ -7,6 +6,6 @@ const { withNativeWind } = require('nativewind/metro')
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = mergeConfig(getDefaultConfig(__dirname), {});
+const config = {};
 
-module.exports = withNativeWind(config, { input: './global.css'})
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
