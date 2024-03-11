b4da7b5d230e0e20531ba9eea7a5854ac1708576
Ajay Sharma <ajshrmaofficial@gmail.com>
2024-03-11 11:27:40 +0530

    Refact: changed stack navigator types to bottom tab navigator types

--- packages/app/src/utility/navigation/NavigationStackTypes.ts
+++ packages/app/src/utility/navigation/NavigationStackTypes.ts
@@ -1,10 +1,16 @@
+import { BottomTabScreenProps } from "@react-navigation/bottom-tabs";
 import type { NativeStackScreenProps } from "@react-navigation/native-stack";
 
 export type NavStackParamList = {
     Login: undefined;
     Register: undefined;
-    Guftgu: undefined;
-    Mehfil: undefined;
 };
 
+export type NavTabParamList = {
+    Guftgu: undefined;
+    Mehfil: undefined;
+    Map: undefined;
+}
+
 export type NavStackProps<T extends keyof NavStackParamList> = NativeStackScreenProps<NavStackParamList, T>;
+export type NavTabProps<T extends keyof NavTabParamList> = BottomTabScreenProps<NavTabParamList, T>;
