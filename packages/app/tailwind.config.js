5b6d205fcf3150b2ca4b8979c04b91375ebb3524
Ajay Sharma <ajshrmaofficial@gmail.com>
2024-03-11 11:15:26 +0530

    Chore: nativewind v2 tailwindcss config

--- packages/app/tailwind.config.js
+++ packages/app/tailwind.config.js
@@ -1,10 +1,9 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
+
 module.exports = {
   content: ["./App.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
-  presets: [require("nativewind/preset")],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
