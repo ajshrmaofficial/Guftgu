72964dd923ade1722b452aa0d5d7f91547ab004d
Ajay Sharma <ajshrmaofficial@gmail.com>
2024-03-20 18:49:55 +0530

    Refact: replaced hardcoded server address to SERVER_IP env variable and added reconnection config

--- packages/app/src/utility/socket/socketConfig.ts
+++ packages/app/src/utility/socket/socketConfig.ts
@@ -1,13 +1,16 @@
 import { io } from "socket.io-client";
+import { SERVER_IP } from "@env";
 
-const URL = "https://backendapi-git-9e847d31bc95.herokuapp.com"
+const URL = SERVER_IP;
 const chatSocket = io(URL + '/chat', {   // TODO: To decide whether to use single instance of socket for mehfil and guftgu or just stick with one...
     autoConnect: false,
     withCredentials: true,
+    reconnection: true,
+    reconnectionAttempts: Infinity,
 })
 
 function setSocketUsername(username: string){
-    chatSocket.auth = {username}
+    chatSocket.auth = {username};
 }
 
 function connectChatSocket(){
