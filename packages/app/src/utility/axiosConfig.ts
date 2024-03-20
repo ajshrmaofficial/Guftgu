3fea85732b0692f702d6dc2d5158494d00ee66f5
Ajay Sharma <ajshrmaofficial@gmail.com>
2024-03-20 18:46:55 +0530

    Refact: removed hardcoded serverIP and added SERVER_IP env variable

--- packages/app/src/utility/axiosConfig.ts
+++ packages/app/src/utility/axiosConfig.ts
@@ -1,9 +1,8 @@
 import axios, { AxiosInstance } from "axios";
 import {SERVER_IP} from "@env";
 
-console.log("SERVER_IP:", SERVER_IP);
 const server: AxiosInstance = axios.create({
-    baseURL: "https://backendapi-git-9e847d31bc95.herokuapp.com",
+    baseURL: SERVER_IP,
     headers: {
         "Content-Type": "application/json",
     }
