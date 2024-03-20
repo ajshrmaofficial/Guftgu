287c601833b7c3e87d7cda92221659290c8db101
Ajay Sharma <ajshrmaofficial@gmail.com>
2024-03-20 18:44:00 +0530

    Chore: watermelonDB configurations done

--- packages/app/babel.config.js
+++ packages/app/babel.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
-  plugins: ['module:react-native-dotenv', 'nativewind/babel'],
+  plugins: ['module:react-native-dotenv', 'nativewind/babel', ["@babel/plugin-proposal-decorators", { "legacy": true }]],
 };
