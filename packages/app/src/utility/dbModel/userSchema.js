990a812ebeec2b3b0ce7d3c60cecc55821b9af41
Ajay Sharma <ajshrmaofficial@gmail.com>
2024-03-29 00:27:52 +0530

    feat: watermelonDB user & message schema added

---
+++ packages/app/src/utility/dbModel/userSchema.js
@@ -0,0 +1,25 @@
+import { appSchema, tableSchema } from "@nozbe/watermelondb";
+
+const userSchema = appSchema({
+    version: 1,
+    tables: [
+        tableSchema({
+            name: "users",
+            columns: [
+                {name: 'username', type: 'string'},
+                {name: 'phone', type: 'string'},
+            ]
+        }),
+        tableSchema({
+            name: "messages",
+            columns: [
+                {name: 'sender_username', type: 'string', isIndexed: true},
+                {name: 'receiver_username', type: 'string', isIndexed: true},
+                {name: 'message', type: 'string'},
+                {name: 'created_at', type: 'number'},
+            ]
+        })
+    ]
+})
+
+export default userSchema;
