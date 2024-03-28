477c54ef6402f2e25dd8dd1263ad45d49e120847
Ajay Sharma <ajshrmaofficial@gmail.com>
2024-03-29 00:26:54 +0530

    chore: MainApplication.kt config for watermelonDB JSI

--- packages/app/android/app/src/main/java/com/guftgu/MainApplication.kt
+++ packages/app/android/app/src/main/java/com/guftgu/MainApplication.kt
@@ -11,6 +11,8 @@ import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.react.flipper.ReactNativeFlipper
 import com.facebook.soloader.SoLoader
+import com.nozbe.watermelondb.jsi.WatermelonDBJSIPackage
+import com.facebook.react.bridge.JSIModulePackage
 
 class MainApplication : Application(), ReactApplication {
 
@@ -28,6 +30,10 @@ class MainApplication : Application(), ReactApplication {
 
         override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
         override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
+
+        override fun getJSIModulePackage(): JSIModulePackage {
+          return WatermelonDBJSIPackage()
+        }
       }
 
   override val reactHost: ReactHost
