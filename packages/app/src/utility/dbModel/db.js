dd236f3a2e38329c1d2dbe9b7582c56eb1045b76
Ajay Sharma <ajshrmaofficial@gmail.com>
2024-03-29 00:29:02 +0530

    feat: Message Database -> implemented watermelonDB config

---
+++ packages/app/src/utility/dbModel/db.js
@@ -0,0 +1,42 @@
+import {Database, Q} from '@nozbe/watermelondb';
+import SQLiteAdapter from '@nozbe/watermelondb/adapters/sqlite';
+import userSchema from './userSchema';
+import {User, Message} from './dbModel';
+
+const adapter = new SQLiteAdapter({
+  dbName: 'UserData ',
+  schema: userSchema,
+  jsi: true,
+  onSetUpError: error => console.log('Problem setting up database', error),
+});
+
+const database = new Database({
+  adapter,
+  modelClasses: [User, Message],
+});
+
+const saveChat = async (senderUsername, receiverUsername, messageText) => {
+  const messageCollection = database.collections.get('messages');
+
+  await database.write(async () => {
+    await messageCollection
+      .create(message => {
+        message.senderUsername = senderUsername;
+        message.receiverUsername = receiverUsername;
+        message.message = messageText;
+      });
+  });
+};
+
+const getChats = async (otherUsername, page, pageSize) => {
+  const messageCollection = database.collections.get('messages');
+  const messages = await messageCollection.query(
+    Q.unsafeSqlQuery(
+      `SELECT * FROM messages WHERE (sender_username = ? AND receiver_username = ?) OR (sender_username = ? AND receiver_username = ?) ORDER BY created_at ASC LIMIT ? OFFSET ?`, ["me", otherUsername, otherUsername, "me", pageSize, page * pageSize]
+    )
+  ).fetch();
+
+  return messages;
+};
+
+export {saveChat, getChats};
