7a185c1e8f454128b461642c176983580a8131bd
Ajay Sharma <ajshrmaofficial@gmail.com>
2024-03-29 19:28:28 +0530

    chore: Update socket configuration to use different server IPs based on environment

--- packages/app/src/utility/socket/socketConfig.ts
+++ packages/app/src/utility/socket/socketConfig.ts
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
-import { SERVER_IP } from "@env";
+import { DEV_SERVER_IP, PROD_SERVER_IP, ENV } from "@env";
 
-const URL = SERVER_IP;
+const URL = ENV==="development" ? DEV_SERVER_IP : PROD_SERVER_IP;
 const chatSocket = io(URL + '/chat', {   // TODO: To decide whether to use single instance of socket for mehfil and guftgu or just stick with one...
     autoConnect: false,
     withCredentials: true,
