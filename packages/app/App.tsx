dbcb6d1fbcd0ef7360713e560dd93958f8f5e003
Ajay Sharma <ajshrmaofficial@gmail.com>
2024-04-10 21:47:54 +0530

    refact: migrated from context to redux

--- packages/app/App.tsx
+++ packages/app/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
-import {AuthProvider} from './src/utility/context/AuthContext';
+// import {AuthProvider} from './src/utility/context/AuthContext';
 import Navigator from './src/utility/navigation/Navigator';
-import {AppThemeProvider} from './src/utility/context/ThemeContext';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
-import {UserProvider} from './src/utility/context/UserContext';
+// import {UserProvider} from './src/utility/context/UserContext';
+import {Provider} from 'react-redux';
+import {store} from './src/utility/redux/rootStore';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import {BottomSheetModalProvider} from '@gorhom/bottom-sheet';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -19,15 +22,15 @@ const queryClient = new QueryClient({
 
 function App(): React.JSX.Element {
   return (
-    <QueryClientProvider client={queryClient}>
-      <AppThemeProvider>
-        <AuthProvider>
-          <UserProvider>
+    <GestureHandlerRootView style={{flex: 1}}>
+      <BottomSheetModalProvider>
+        <Provider store={store}>
+          <QueryClientProvider client={queryClient}>
             <Navigator />
-          </UserProvider>
-        </AuthProvider>
-      </AppThemeProvider>
-    </QueryClientProvider>
+          </QueryClientProvider>
+        </Provider>
+      </BottomSheetModalProvider>
+    </GestureHandlerRootView>
   );
 }
 
