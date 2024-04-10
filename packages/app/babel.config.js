fb342b822d888c25ffa34f1a597b5ae04b1b37b1
Ajay Sharma <ajshrmaofficial@gmail.com>
2024-04-10 21:51:59 +0530

    chore: added reanimated plugin to babel config

--- packages/app/babel.config.js
+++ packages/app/babel.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
-  plugins: ['module:react-native-dotenv', 'nativewind/babel', ["@babel/plugin-proposal-decorators", { "legacy": true }]],
+  plugins: ['module:react-native-dotenv', 'nativewind/babel', ["@babel/plugin-proposal-decorators", { "legacy": true }], 'react-native-reanimated/plugin'],
 };
