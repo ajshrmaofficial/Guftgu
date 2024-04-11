e4fd70bd10bf2ac7e592be7f5bfbb964777d007e
Ajay Sharma <ajshrmaofficial@gmail.com>
2024-04-11 15:51:53 +0530

    refact: Refactor Profile screen to use name instead of username for better clarity

--- packages/app/src/screens/Profile.tsx
+++ packages/app/src/screens/Profile.tsx
@@ -5,15 +5,15 @@ import { useTheme } from "@react-navigation/native";
 
 function Profile(): React.JSX.Element {
     const {colors} = useTheme();
-    const {username} = useAuth();
-    const myUsername = username || "User";
+    const {name} = useAuth();
+    const myName = name || "User";
 
     return(
         <View className="h-full w-full">
             <View className="h-20 w-20 bg-slate-800 self-center items-center justify-center rounded-3xl">
-                <Text className="text-4xl font-bold text-white">{myUsername[0].toLocaleUpperCase()}</Text>
+                <Text className="text-4xl font-bold text-white">{myName[0].toLocaleUpperCase()}</Text>
             </View>
-            <Text style={{color: colors.text}} className="self-center text-2xl font-bold">{username}</Text>
+            <Text style={{color: colors.text}} className="self-center text-2xl font-bold">{myName}</Text>
         </View>
     )
 }
