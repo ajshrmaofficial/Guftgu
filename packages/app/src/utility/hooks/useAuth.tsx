a9b49d3e59ed10b53ff66fff38f53989e1a20dc3
Ajay Sharma <ajshrmaofficial@gmail.com>
2024-04-11 15:54:59 +0530

    fix: Update useAuth hook to include 'name' field in authentication logic

--- packages/app/src/utility/hooks/useAuth.tsx
+++ packages/app/src/utility/hooks/useAuth.tsx
@@ -1,13 +1,14 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useEffect } from "react";
 import { useAppGetState, useAppSetState } from "../redux/useAppState";
-import { setAuthToken, setAuthUsername } from "../redux/authSlice";
+import { setAuthName, setAuthToken, setAuthUsername } from "../redux/authSlice";
 
 
 function useAuth() {
     const setState = useAppSetState();
     const authToken = useAppGetState(state => state.auth.authToken);
     const username = useAppGetState(state => state.auth.username);
+    const name = useAppGetState(state => state.auth.name);
 
     useEffect(() => {
         if(authToken && username) {
@@ -17,15 +18,17 @@ function useAuth() {
       const fetchData = async () => {
         const authToken = await AsyncStorage.getItem('authToken');
         const username = await AsyncStorage.getItem('username');
-        if (authToken && username) {
+        const name = await AsyncStorage.getItem('name');
+        if (authToken && username && name) {
           setState(setAuthToken(authToken));
           setState(setAuthUsername(username));
+          setState(setAuthName(name));
         }
       };
       fetchData();
     }, []);
   
-    return {authToken, username};
+    return {authToken, username, name};
 }
 
 export default useAuth;
