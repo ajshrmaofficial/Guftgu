5f604ca579c767bd2a69c1365caef4113f3d61e1
Ajay Sharma <ajshrmaofficial@gmail.com>
2024-04-11 15:53:32 +0530

    refact: Refactor Register screen to validate and submit user credentials

--- packages/app/src/screens/Register.tsx
+++ packages/app/src/screens/Register.tsx
@@ -2,11 +2,15 @@ import React, {useState} from 'react';
 import {Text, TextInput, TouchableOpacity, View} from 'react-native';
 import { useAuthFunctions } from '../utility/definitionStore';
 import {AuthStackProps} from '../utility/navigation/NavigationStackTypes';
+import Loader from '../components/Loader';
+import { useTheme } from '@react-navigation/native';
 
 function Register({navigation}: AuthStackProps<"Register">): React.JSX.Element {
   const {register} = useAuthFunctions();
+  const {colors} = useTheme();
   const [username, setUsername] = useState<string>('');
   const [password, setPassword] = useState<string>('');
+  const [name, setName] = useState<string>('');
   const [loading, setLoading] = useState<boolean>(false);
   const [err, setErr] = useState<string>('');
   const [confirmPassword, setConfirmPassword] = useState<string>('');
@@ -14,6 +18,8 @@ function Register({navigation}: AuthStackProps<"Register">): React.JSX.Element {
   const [hasUpperCase, setHasUpperCase] = useState<boolean>(false);
   const [hasNumber, setHasNumber] = useState<boolean>(false);
   const [hasSpecialChar, setHasSpecialChar] = useState<boolean>(false);
+  const [usernameIsValid, setUsernameIsValid] = useState<boolean>(false);
+  const [nameIsValid, setNameIsValid] = useState<boolean>(false);
 
   const validatePassword = (text: string): void => {
     setPassword(text);
@@ -21,6 +27,16 @@ function Register({navigation}: AuthStackProps<"Register">): React.JSX.Element {
     setHasUpperCase(/[A-Z]/.test(text));
     setHasNumber(/\d/.test(text));
     setHasSpecialChar(/[!@#$%^&*()_+]/.test(text));
+  }
+
+  const validateUsername = (text: string): void => {
+    setUsername(text);
+    setUsernameIsValid(/^[a-z0-9._-]{3,}$/.test(text))
+  }
+
+  const validateName = (text: string): void => {
+    setName(text);
+    setNameIsValid(/^[a-zA-Z-' ]*[a-zA-Z-' ][a-zA-Z-' ]*$/.test(text))
   }
 
   const comparePasswords = (text: string): void => {
@@ -31,14 +47,34 @@ function Register({navigation}: AuthStackProps<"Register">): React.JSX.Element {
   }
 
   const submitCredentials = async() => {
+    if(!name || !username || !password || !confirmPassword){
+      setErr("Enter all details !!");
+      return;
+    }
+    if(!hasLowerCase || !hasUpperCase || !hasNumber || !hasSpecialChar){
+      setErr("Enter valid password !!!");
+      return;
+    }
+    if(!nameIsValid || !name.trim()){
+      setErr("Enter valid name !!!");
+      return;
+    }
+    if(!usernameIsValid){
+      setErr("Enter valid username !!!");
+      return;
+    }
     if(password!==confirmPassword){
+        setErr("Passwords do not match !!!");
         return;
     }
-    const res = await register(username, password);
+    setLoading(true);
+    const res = await register(username, password, name);
     if(res){
       setErr(res);
       return;
     }
+    setLoading(false);
+    setErr("Redirecting to Login...");
     setTimeout(()=>{
         navigation.navigate("Login")
     }, 1500);
@@ -51,43 +87,58 @@ function Register({navigation}: AuthStackProps<"Register">): React.JSX.Element {
   return (
     <>
       {loading && (
-        <View>
-          <Text className="text-4xl">Loading...</Text>
-        </View>
+        <Loader/>
       )}
       {!loading && (
         <View className="items-center">
-          <Text className="text-4xl absolute m-3 self-start">Register</Text>
+          <Text style={{color: colors.text}} className="text-4xl absolute m-3 self-start">Register</Text>
           <View className="w-2/3 h-full justify-center items-center">
-            <TextInput
-              className="border border-white rounded-md w-full mb-4"
-              placeholder="Username"
-              value={username}
-              onChangeText={text => setUsername(text)}
+          <TextInput
+              className="border rounded-md w-full mb-4"
+              style={{color: colors.text}}
+              placeholder="Name"
+              placeholderTextColor={colors.text}
+              value={name}
+              onChangeText={text => validateName(text)}
             />
             <TextInput
-              className="border border-white rounded-md w-full mb-5"
+              className="border rounded-md w-full mb-4"
+              style={{color: colors.text}}
+              placeholder="Username"
+              placeholderTextColor={colors.text}
+              value={username}
+              onChangeText={text => validateUsername(text)}
+            />
+            <TextInput
+              className="border rounded-md w-full mb-4"
+              style={{color: colors.text}}
               placeholder="Password"
+              placeholderTextColor={colors.text}
               value={password}
               onChangeText={text => validatePassword(text)}
+              secureTextEntry={true}
             />
             <TextInput
-              className="border border-white rounded-md w-full mb-5"
+              className="border rounded-md w-full mb-5"
+              style={{color: colors.text}}
               placeholder="Confirm Password"
+              placeholderTextColor={colors.text}
               value={confirmPassword}
               onChangeText={text => comparePasswords(text)}
+              secureTextEntry={true}
             />
             <TouchableOpacity
-              className="border border-white rounded-md w-2/3 p-2 items-center"
+              className="border rounded-md w-2/3 p-2 items-center"
               onPress={!loading && submitCredentials}>
-              <Text>Submit</Text>
+              <Text style={{color: colors.text}}>Submit</Text>
             </TouchableOpacity>
-            <Text className="flex-row justify-center m-2">
+            <Text style={{color: colors.text}}>
               Login Instead?{' '}
-              <TouchableOpacity onPress={navigateLogin}>
-                <Text>Login</Text>
-              </TouchableOpacity>
             </Text>
+              <TouchableOpacity onPress={navigateLogin}>
+                <Text className='text-blue-700'>Login</Text>
+              </TouchableOpacity>
+            <Text className="text-red-500">{err}</Text>
           </View>
         </View>
       )}
