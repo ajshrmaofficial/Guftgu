ad25655e50c722f636cec19cfab1f9080ffb771d
Ajay Sharma <ajshrmaofficial@gmail.com>
2024-04-11 15:54:02 +0530

    refact: Refactor axiosConfig.ts to use ENV variable for baseURL

--- packages/app/src/utility/axiosConfig.ts
+++ packages/app/src/utility/axiosConfig.ts
@@ -2,7 +2,7 @@ import axios, { AxiosInstance } from "axios";
 import {DEV_SERVER_IP, PROD_SERVER_IP, ENV} from "@env";
 
 const server: AxiosInstance = axios.create({
-    baseURL: ENV==="development" ? DEV_SERVER_IP : PROD_SERVER_IP,
+    baseURL: ENV==="prod" ? PROD_SERVER_IP: DEV_SERVER_IP,
     headers: {
         "Content-Type": "application/json",
     }
