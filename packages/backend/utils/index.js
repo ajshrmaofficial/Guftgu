87a0175aef7d26bf024d7d671b562be5c93e778f
Ajay Sharma <ajshrmaofficial@gmail.com>
2024-09-07 13:15:56 +0530

    refactor: Update utils index.js to include renamed errorHandlerMiddleware

---
+++ packages/backend/utils/index.js
@@ -0,0 +1,11 @@
+const AppError = require('./appErrorClass');
+const errorHandlerMiddleware = require('./errorHandler'); //TODO: Have to rename errorHandler file to errorHandlerMiddleware
+const tryCatch = require('./tryCatch')
+const firebase = require('./firebase');
+
+module.exports = {
+    AppError,
+    errorHandlerMiddleware,
+    tryCatch,
+    firebase
+};
