dd20b3bd43fa752d9716dc559e724c16d6f69cf0
Ajay Sharma <ajshrmaofficial@gmail.com>
2024-09-07 13:16:20 +0530

    refactor: Add schema models for friendship, message, and user
    This commit adds the schema models for friendship, message, and user to the backend. The models are imported and exported in the index.js file.

---
+++ packages/backend/schema/index.js
@@ -0,0 +1,9 @@
+const friendshipModel = require('./friendShipSchema');
+const messageModel = require('./messageSchema')
+const userModel = require('./userSchema')
+
+module.exports = {
+    friendshipModel,
+    messageModel,
+    userModel
+};
