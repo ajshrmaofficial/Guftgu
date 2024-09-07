428acac20d93aa89bd3dd3f117eeec8dc2271d62
Ajay Sharma <ajshrmaofficial@gmail.com>
2024-09-07 16:55:13 +0530

    feat: added request authorization for socket events

---
+++ packages/backend/handlers/socketAuth.js
@@ -0,0 +1,18 @@
+// TODO: make a new folder middlewares and move all middlewares to middleware folder...
+const jwt = require("jsonwebtoken");
+
+module.exports = (socket, next) => {
+  const token = socket.handshake.auth.token;
+  if (!token) {
+    return next(new Error("Authentication error: Token is missing"));
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) {
+      return next(new Error("Authentication error: Invalid token"));
+    }
+    const {username} = decoded;
+    socket.username = username;
+    next();
+  });
+};
