cd26af3b6ae912a98e5b8c511c70a6316fcecaec
Ajay Sharma <ajshrmaofficial@gmail.com>
2024-09-16 23:19:58 +0530

    feat: migrated useSocketEvents to use socket event model

--- packages/app/src/utility/socket/useSocketEvents.ts
+++ packages/app/src/utility/socket/useSocketEvents.ts
@@ -1,12 +1,17 @@
-import {useEffect} from 'react';
+import {useCallback, useEffect, useMemo} from 'react';
 import {chatSocket} from './socketConfig';
 
-export interface Event {
+export interface ReceiveEvent {
   name: string;
   handler(...args: any[]): any;
 }
 
-export function useSocketEvents(events: Event[]) {
+export interface SendEvent {
+  name: string;
+  prepare?: (...args: any[]) => any;
+}
+
+export function useSocketReceiveEvents(events: ReceiveEvent[]) {
   useEffect(() => {
     for (const event of events) {
       chatSocket.on(event.name, event.handler);
@@ -18,4 +23,29 @@ export function useSocketEvents(events: Event[]) {
       }
     };
   }, []);
+}
+
+// export function useSocketSendEvents<T extends Record<string, SendEvent>>(events: T) {
+//   const sendEventHandlers = useMemo(() => {
+//     return Object.entries(events).reduce((acc, [key, event]) => {
+//       acc[key as keyof T] = useCallback((...args: any[]) => {
+//         const data = event.prepare ? event.prepare(...args) : args;
+//         chatSocket.emit(event.name, ...data);
+//       }, [event.name, event.prepare]);
+//       return acc;
+//     }, {} as { [K in keyof T]: (...args: any[]) => void });
+//   }, [events]);
+
+//   return sendEventHandlers;
+// }
+
+export function useSocketSendEvents(events: SendEvent[]){
+  const handlers: Record<string, (...args: any[])=>void> = {}
+  events.map(value => {
+    handlers[value.name] = (...args: any[]) => {
+      const data = value.prepare ? value.prepare(...args) : args;
+      chatSocket.emit(value.name, data);
+    } 
+  })
+  return handlers;
 }
