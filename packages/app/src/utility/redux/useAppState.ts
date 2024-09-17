be36cba89065b61d1da969bddfa023fe80a9fbdc
Ajay Sharma <ajshrmaofficial@gmail.com>
2024-09-17 18:47:36 +0530

    refactor: removed old redux files

--- packages/app/src/utility/redux/useAppState.ts
+++
@@ -1,5 +0,0 @@
-import { useDispatch, useSelector, TypedUseSelectorHook } from "react-redux";
-import { RootStateType, AppDispatchType } from "./rootStore";
-
-export const useAppSetState: () => AppDispatchType = useDispatch;
-export const useAppGetState: TypedUseSelectorHook<RootStateType> = useSelector;
