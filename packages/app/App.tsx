871bb3e48adc4cce535902c40c36187c9a3052c3
Ajay Sharma <ajshrmaofficial@gmail.com>
2024-09-18 09:17:11 +0530

    refactor: removed redundant redux code

--- packages/app/App.tsx
+++ packages/app/App.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
-// import {AuthProvider} from './src/utility/context/AuthContext';
 import Navigator from './src/utility/navigation/Navigator';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
-// import {UserProvider} from './src/utility/context/UserContext';
-import {Provider} from 'react-redux';
-import {store} from './src/utility/redux/rootStore';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import {BottomSheetModalProvider} from '@gorhom/bottom-sheet';
 
@@ -24,11 +20,9 @@ function App(): React.JSX.Element {
   return (
     <GestureHandlerRootView style={{flex: 1}}>
       <BottomSheetModalProvider>
-        <Provider store={store}>
-          <QueryClientProvider client={queryClient}>
-            <Navigator />
-          </QueryClientProvider>
-        </Provider>
+        <QueryClientProvider client={queryClient}>
+          <Navigator />
+        </QueryClientProvider>
       </BottomSheetModalProvider>
     </GestureHandlerRootView>
   );
