7acd009b92957572793d513789557d85feb93dba
Ajay Sharma <ajshrmaofficial@gmail.com>
2024-12-01 00:56:55 +0530

    feat: update Profile screen to use custom theme colors for text styling

--- packages/app/src/screens/Profile.tsx
+++ packages/app/src/screens/Profile.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { Text, View } from "react-native";
-import { useTheme } from "@react-navigation/native";
 import useUserStore from "../utility/store/userStore";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { version } from '../../package.json';
+import getThemeColors from "../utility/theme";
 
 function Profile(): React.JSX.Element {
-    const {colors} = useTheme();
+    const {text} = getThemeColors();
     const name = useUserStore(state => state.name);
     const username = useUserStore(state => state.username);
     const myName = name || "User";
@@ -17,12 +17,12 @@ function Profile(): React.JSX.Element {
             <View className="h-20 w-20 bg-slate-800 self-center items-center justify-center rounded-3xl">
                 <Text className="text-4xl font-bold text-white">{myName[0].toLocaleUpperCase()}</Text>
             </View>
-            <Text style={{color: colors.text}} className="self-center text-2xl font-bold">{myName}</Text>
-            <Text style={{color: colors.text}} className="self-center text-md text-gray-700">@{username}</Text>
+            <Text className={`${text.primary.tailwind} self-center text-2xl font-bold`}>{myName}</Text>
+            <Text className={`${text.secondary.tailwind} self-center text-md text-gray-700`}>@{username}</Text>
             <View className="absolute bottom-12 self-center items-center">
-                <Text className="text-xs text-gray-700 font-semibold">v{version}</Text>
-                <Text className="text-xs text-gray-700 font-semibold">Made with ❤️</Text>
-                <Text className="text-xs text-gray-700 font-semibold">by Ajay Sharma</Text>
+                <Text className={`text-xs ${text.accent.tailwind} font-semibold`}>v{version}</Text>
+                <Text className={`text-xs ${text.accent.tailwind} font-semibold`}>Made with ❤️</Text>
+                <Text className={`text-xs ${text.accent.tailwind} font-semibold`}>by Ajay Sharma</Text>
             </View>
         </View>
     )
