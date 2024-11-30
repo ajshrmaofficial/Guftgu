f46c7e4bf248ee677b6afa6e0d5de1470f7e8cb9
Ajay Sharma <ajshrmaofficial@gmail.com>
2024-12-01 01:29:33 +0530

    feat: enhance Firebase initialization to support environment variables for service account configuration

--- packages/backend/utils/firebase/index.js
+++ packages/backend/utils/firebase/index.js
@@ -1,9 +1,26 @@
 const firebase = require("firebase-admin");
-
 const serviceAccount = require("./serviceAccountKeys.json");
 
-firebase.initializeApp({
-    credential: firebase.credential.cert(serviceAccount)
-})
+if (serviceAccount) {
+    firebase.initializeApp({
+        credential: firebase.credential.cert(serviceAccount)
+    });
+} else {
+    firebase.initializeApp({
+        credential: firebase.credential.cert({
+            type: process.env.SERVICE_ACCOUNT_TYPE,
+            project_id: process.env.SERVICE_ACCOUNT_PROJECT_ID,
+            private_key_id: process.env.SERVICE_ACCOUNT_PRIVATE_KEY_ID,
+            private_key: process.env.SERVICE_ACCOUNT_PRIVATE_KEY,
+            client_email: process.env.SERVICE_ACCOUNT_CLIENT_EMAIL,
+            client_id: process.env.SERVICE_ACCOUNT_CLIENT_ID,
+            auth_uri: process.env.SERVICE_ACCOUNT_AUTH_URI,
+            token_uri: process.env.SERVICE_ACCOUNT_TOKEN_URI,
+            auth_provider_x509_cert_url: process.env.SERVICE_ACCOUNT_AUTH_PROVIDER_X509_CERT_URL,
+            client_x509_cert_url: process.env.SERVICE_ACCOUNT_CLIENT_X509_CERT_URL,
+            universe_domain: process.env.SERVICE_ACCOUNT_UNIVERSE_DOMAIN,
+        }),
+    });
+}
 
 module.exports = firebase;
