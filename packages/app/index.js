9b56257ededcb88b0bd4917dfb52a4918e5d4c2e
Ajay Sharma <ajshrmaofficial@gmail.com>
2024-12-01 00:53:34 +0530

    feat: initialize notifications in the app entry point

--- packages/app/index.js
+++ packages/app/index.js
@@ -5,5 +5,9 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import { initializeNotifications } from './src/utility/hooks/useNotification';
+// import messaging from '@react-native-firebase/messaging';
+
+initializeNotifications();
 
 AppRegistry.registerComponent(appName, () => App);
