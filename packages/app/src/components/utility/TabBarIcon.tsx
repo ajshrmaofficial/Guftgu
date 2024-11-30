44a011fb4e53254c5c62d277c2a68a9034705307
Ajay Sharma <ajshrmaofficial@gmail.com>
2024-12-01 00:57:19 +0530

    feat: refactor TabBarIcon component to use MaterialIcons and theme-based styling

--- packages/app/src/components/utility/TabBarIcon.tsx
+++ packages/app/src/components/utility/TabBarIcon.tsx
@@ -1,20 +1,16 @@
 import React from "react";
-import { StyleSheet } from "react-native";
-import { SvgProps } from "react-native-svg";
+import { View } from "react-native";
+import Icon from "react-native-vector-icons/MaterialIcons";
+import getThemeColors from "../../utility/theme";
 
-function TabBarIcon(props: {color: string, size: number, icon: React.FC<SvgProps>}): React.JSX.Element {
-    const Icon = props.icon;
-    
-    const styles = StyleSheet.create({
-        icon: {
-            color: props.color,
-            width: props.size,
-            height: props.size
-        }
-    })
+function TabBarIcon(props: {size: number, isFocused: boolean, name: string}): React.JSX.Element {
+    const {background, icons} = getThemeColors();
+    const iconName = props.name === 'Guftgu' ? 'chat' : props.name === 'Mehfil' ? 'group' : 'map';
 
     return (
-        <Icon style={styles.icon}/>
+        <View className={`py-1 px-5 rounded-3xl ${props.isFocused ? background.accent.tailwind : ''}`}>
+            <Icon name={iconName} size={props.size} color={props.isFocused ? icons.accent.hex : icons.primary.hex} />  
+        </View>
     )
 }
 
