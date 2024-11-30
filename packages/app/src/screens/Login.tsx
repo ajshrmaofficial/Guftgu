426a689b1743bd13a245ee22b8603b2ba9a78a7c
Ajay Sharma <ajshrmaofficial@gmail.com>
2024-12-01 00:57:00 +0530

    feat: enhance Login screen with custom theme colors and safe area insets

--- packages/app/src/screens/Login.tsx
+++ packages/app/src/screens/Login.tsx
@@ -3,7 +3,8 @@ import {Text, TextInput, TouchableOpacity, View} from 'react-native';
 import { useAuthFunctions } from '../utility/definitionStore';
 import {AuthStackProps} from '../utility/navigation/NavigationStackTypes';
 import Loader from '../components/utility/Loader';
-import { useTheme } from '@react-navigation/native';
+import getThemeColors from '../utility/theme';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 interface LoginFormData{
   username: string
@@ -20,7 +21,8 @@ function Login({navigation}: AuthStackProps<"Login">): React.JSX.Element {
   });
   const {username, password, error} = loginFormData;
   const [loading, setLoading] = useState<boolean>(false);
-  const {colors} = useTheme();
+  const {text} = getThemeColors();
+  const insets = useSafeAreaInsets();
 
   const submitCredentials = async () => {
 
@@ -36,48 +38,41 @@ function Login({navigation}: AuthStackProps<"Login">): React.JSX.Element {
     navigation.navigate('Register');
   };
 
+  if (loading) return <Loader />;
+
   return (
-    <>
-      {loading && (
-        <Loader/>
-      )}
-      {!loading && (
-        <View className="items-center">
-          <Text style={{color: colors.text}} className="text-4xl absolute m-3 self-start">Login</Text>
-          <View className="w-2/3 h-full justify-center items-center">
-            <TextInput
-              className="border rounded-md w-full mb-4"
-              style={{color: colors.text}}
-              placeholder="Username"
-              placeholderTextColor={colors.text}
-              value={username}
-              onChangeText={text => setLoginFormData({...loginFormData, username: text})}
-            />
-            <TextInput
-              className="border rounded-md w-full mb-5"
-              style={{color: colors.text}}
-              placeholder="Password"
-              placeholderTextColor={colors.text}
-              value={password}
-              onChangeText={text => setLoginFormData({...loginFormData, password: text})}
-              secureTextEntry={true}
-            />
-            <TouchableOpacity
-              className="border rounded-md w-2/3 p-2 items-center"
-              onPress={!loading && submitCredentials}>
-              <Text style={{color: colors.text}}>Submit</Text>
-            </TouchableOpacity>
-            <Text style={{color: colors.text}}>
-              Doesn't have an account?{' '}
-            </Text>
-              <TouchableOpacity onPress={navigateSignup}>
-                <Text className='text-blue-700'>SignUp</Text>
-              </TouchableOpacity>
-            <Text className="text-red-500">{error}</Text>
-          </View>
-        </View>
-      )}
-    </>
+    <View className="items-center flex-1" style={{paddingTop: insets.top}}>
+      <Text className={`text-4xl absolute self-start ${text.primary.tailwind} mx-3 my-6`}>Login</Text>
+      <View className="w-2/3 h-full justify-center items-center">
+        <TextInput
+          className={`border rounded-md w-full mb-4 ${text.primary.tailwind}`}
+          placeholder="Username"
+          placeholderTextColor={text.primary.hex}
+          value={username}
+          onChangeText={text => setLoginFormData({...loginFormData, username: text})}
+        />
+        <TextInput
+          className={`border rounded-md w-full mb-5 ${text.primary.tailwind}`}
+          placeholder="Password"
+          placeholderTextColor={text.primary.hex}
+          value={password}
+          onChangeText={text => setLoginFormData({...loginFormData, password: text})}
+          secureTextEntry={true}
+        />
+        <TouchableOpacity
+          className="border rounded-md w-2/3 p-2 items-center"
+          onPress={!loading && submitCredentials}>
+          <Text className={`${text.primary.tailwind}`}>Submit</Text>
+        </TouchableOpacity>
+        <Text className={`${text.primary.tailwind}`}>
+          Doesn't have an account?{' '}
+        </Text>
+          <TouchableOpacity onPress={navigateSignup}>
+            <Text className={`${text.accent.tailwind}`}>SignUp</Text>
+          </TouchableOpacity>
+        <Text className="text-red-500">{error}</Text>
+      </View>
+    </View>
   );
 }
 
